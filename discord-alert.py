a1ebd2acb0f377069aa428bb6e65503f3b164588
Amin <amin.mafn@gmail.com>
2022-04-10 20:16:53 +0430

    commit1

--- discord-alert.py
+++ discord-alert.py
@@ -2,3 +2,9 @@ import pandas as pd
 import pandas_ta as ta
 import ccxt
 
+exchange = ccxt.coinex()
+
+bars = exchange.fetch_ohlcv('ETH/USDT', timeframe='5m', limit=500)
+df = pd.DataFrame(bars, columns=['time', 'open', 'high', 'low', 'clode', 'volume'])
+
+print(df)
